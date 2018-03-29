26e089164baf6888688591aca4a2f825b15ca462
Pawel Ratajczak <pawel@lampdevelopment.co.uk>
2018-03-29 16:04:37 +0100

    Working with 2 dimensional input

--- test.js
+++ test.js
@@ -4,16 +4,24 @@ var convnetjs = require('convnetjs');
 var net = new convnetjs.Net();
 var d = 5;
 var layer_defs = [];
-layer_defs.push({type:'input', out_sx:1, out_sy:1, out_depth:d});
+layer_defs.push({type:'input', out_sx:2, out_sy:1, out_depth:d});
 layer_defs.push({type:'fc', num_neurons:30, activation:'sigmoid'});
-layer_defs.push({type:'regression', num_neurons:1});
+layer_defs.push({type:'regression', num_neurons:2});
 var net = new convnetjs.Net();
 net.makeLayers(layer_defs);
 
+// var my_data = [
+//     0,1,2,3,4,5,6,
+//     0,1,2,3,4,5,6,
+//     0,1,2,3,4,5,6
+// ];
+
 var my_data = [
-    0,1,2,3,4,5,6,
-    0,1,2,3,4,5,6,
-    0,1,2,3,4,5,6
+  [0,3],[1,4],[2,5],[3,6],[4,7],[5,8],[6,9],
+  [0,3],[1,4],[2,5],[3,6],[4,7],[5,8],[6,9],
+  [0,3],[1,4],[2,5],[3,6],[4,7],[5,8],[6,9],
+  [0,3],[1,4],[2,5],[3,6],[4,7],[5,8],[6,9],
+  [0,3],[1,4],[2,5],[3,6],[4,7],[5,8],[6,9]
 ];
 
 
@@ -21,30 +29,43 @@ var my_data = [
 var trainer = new convnetjs.SGDTrainer(net, {learning_rate:0.01, momentum:0.2, batch_size:1, l2_decay:0.001});
 
 var learn = function () {
-  for(var j = 0; j < 500; j++){
+  for(var j = 0; j < 1000; j++){
     for (var i = 0; i < my_data.length - d; i++) {
         var data = my_data.slice(i, i + d);
-        var real_value = [my_data[i + d]];
-        var x = new convnetjs.Vol(data);
+        var real_value = [my_data[i + d]][0];
+        var x = new convnetjs.Vol(2, 1, 5, 0);
+
+        for (var ii = 0; ii < d; ii++) {
+          x.set(0,0,ii,data[ii][0]);
+          x.set(1,0,ii,data[ii][1]);
+        }
+
+
         trainer.train(x, real_value);
         var predicted_values = net.forward(x);
         console.log("data: [" + data + "] -> value: " + real_value);
-        console.log("prediction in learn stage is: " + predicted_values.w[0]);
+        console.log("prediction in learn stage is: " + predicted_values.w[0] + ', ' + predicted_values.w[1]);
     }
   }
 
 }
 
 var predict = function(data){
-  var x = new convnetjs.Vol(data);
+  var x = new convnetjs.Vol(2, 1, 5, 0);
+
+  for (var ii = 0; ii < d; ii++) {
+    x.set(0,0,ii,data[ii][0]);
+    x.set(1,0,ii,data[ii][1]);
+  }
+
   var predicted_value = net.forward(x);
-  return predicted_value.w[0];
+  return predicted_value.w ;
 }
 
 learn();
-var item = [0,1,2,3,4];
-console.log("predicted value for [" + item + "] is: " + predict(item));
-var item = [4,5,6,0,1];
-console.log("predicted value for [" + item + "] is: " + predict(item));
-var item = [6,0,1,2,3];
-console.log("predicted value for [" + item + "] is: " + predict(item));
+var item = [[0,3],[1,4],[2,5],[3,6],[4,7]];
+var predicted = predict(item);
+console.log("predicted value for [" + item + "] is: " + predicted[0] + ', ' + predicted[1]);
+var item = [[6,9],[0,3],[1,4],[2,5],[3,6]];
+var predicted = predict(item);
+console.log("predicted value for [" + item + "] is: " + predicted[0] + ', ' + predicted[1]);
